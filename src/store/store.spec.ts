6b99d18da8652f2e718c60055d89746214182af7
jakob <jakob.fischl@protonmail.com>
2024-04-26 13:48:05 +0200

    - remove basically everything from the store, everything that remained where pointless wrappers

--- src/store/store.spec.ts
+++ src/store/store.spec.ts
@@ -1,4 +1,4 @@
-import {effect, signal, Signal} from '@angular/core';
+import {computed, effect, signal, Signal} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 import {Store} from './store';
 
@@ -10,12 +10,18 @@ class TestStore extends Store<number> {
   public readonly selectorFn = jest.fn(
     (state: number) => state + 1,
   );
-  public readonly selector = this.createSelector(this.selectorFn);
+  public readonly selector = computed(() => {
+    return this.selectorFn(this.state());
+  });
 
-  public readonly computedSelectorFn = jest.fn(
-    (state: number, { increment }: { increment: Signal<number> }) => state + increment(),
+  public readonly selectorWithInputsFn = jest.fn(
+    (increment: Signal<number>) => this.state() + increment(),
   );
-  public readonly computedSelector = this.createComputedSelector(this.computedSelectorFn)
+  public selectorWithInputs(increment: Signal<number>): Signal<number> {
+    return computed(() => {
+      return this.selectorWithInputsFn(increment);
+    });
+  }
 }
 
 describe('Store', () => {
@@ -31,8 +37,8 @@ describe('Store', () => {
         TestBed.runInInjectionContext(() => {
           const result: number[] = [];
 
-          const selector1 = store.selector.inject();
-          const selector2 = store.selector.inject();
+          const selector1 = store.selector;
+          const selector2 = store.selector;
 
           effect(() => {
             result.push(selector1());
@@ -62,8 +68,8 @@ describe('Store', () => {
       it('should unsubscribe from the selector if it is no longer used', () => {
         TestBed.runInInjectionContext(() => {
 
-          const selector1 = store.selector.inject();
-          const selector2 = store.selector.inject();
+          const selector1 = store.selector;
+          const selector2 = store.selector;
 
           effect(() => {
             selector1();
@@ -88,30 +94,6 @@ describe('Store', () => {
         });
       });
     });
-
-    describe('get', () => {
-      it('should return the current state even if nobody is listening', () => {
-        store.update(() => 1);
-        expect(store.selector.get()).toBe(2);
-        expect(store.selector.inject()()).toBe(2);
-        // 2 calls because get does not cache the value
-        expect(store.selectorFn).toHaveBeenCalledTimes(2);
-      });
-
-      it('should not recompute the current state if someone is listening', () => {
-        TestBed.runInInjectionContext(() => {
-          const selector = store.selector.inject();
-
-          effect(() => {
-            selector();
-          });
-          TestBed.flushEffects();
-
-          expect(store.selector.get()).toEqual(1);
-          expect(store.selectorFn).toHaveBeenCalledTimes(1);
-        });
-      });
-    });
   });
 
   describe('computedSelector', () => {
@@ -120,7 +102,7 @@ describe('Store', () => {
         TestBed.runInInjectionContext(() => {
           const result: number[] = [];
 
-          const selector = store.computedSelector.inject({ increment: signal(2) });
+          const selector = store.selectorWithInputs(signal(2));
 
           effect(() => {
             result.push(selector());
@@ -131,7 +113,7 @@ describe('Store', () => {
           TestBed.flushEffects();
 
           expect(result).toEqual([2, 3]);
-          expect(store.computedSelectorFn).toHaveBeenCalledTimes(2);
+          expect(store.selectorWithInputsFn).toHaveBeenCalledTimes(2);
         });
       });
 
@@ -139,7 +121,7 @@ describe('Store', () => {
         TestBed.runInInjectionContext(() => {
           store.update(() => 1);
           TestBed.flushEffects();
-          expect(store.computedSelectorFn).toHaveBeenCalledTimes(0);
+          expect(store.selectorWithInputsFn).toHaveBeenCalledTimes(0);
         });
       });
 
@@ -148,7 +130,7 @@ describe('Store', () => {
           const result: number[] = [];
 
           const increment = signal(2);
-          const selector = store.computedSelector.inject({ increment });
+          const selector = store.selectorWithInputs(increment);
 
           effect(() => {
             result.push(selector());
@@ -161,7 +143,7 @@ describe('Store', () => {
           TestBed.flushEffects();
 
           expect(result).toEqual([2, 3, 4]);
-          expect(store.computedSelectorFn).toHaveBeenCalledTimes(3);
+          expect(store.selectorWithInputsFn).toHaveBeenCalledTimes(3);
         });
       });
 
@@ -172,8 +154,8 @@ describe('Store', () => {
 
           const increment1 = signal(2);
           const increment2 = signal(3);
-          const selector1 = store.computedSelector.inject({ increment: increment1 });
-          const selector2 = store.computedSelector.inject({ increment: increment2 });
+          const selector1 = store.selectorWithInputs(increment1);
+          const selector2 = store.selectorWithInputs(increment2);
 
           effect(() => {
             result1.push(selector1());
@@ -192,13 +174,13 @@ describe('Store', () => {
 
           expect(result1).toEqual([2, 3, 4]);
           expect(result2).toEqual([3, 4, 5]);
-          expect(store.computedSelectorFn).toHaveBeenCalledTimes(6);
+          expect(store.selectorWithInputsFn).toHaveBeenCalledTimes(6);
         });
       });
 
       it('should unsubscribe when it is no longer used', () => {
         TestBed.runInInjectionContext(() => {
-          const selector = store.computedSelector.inject({ increment: signal(2) });
+          const selector = store.selectorWithInputs(signal(2));
 
           effect(() => {
             selector();
@@ -215,46 +197,7 @@ describe('Store', () => {
           store.update(() => 2);
           TestBed.flushEffects();
 
-          expect(store.computedSelectorFn).toHaveBeenCalledTimes(2);
-        });
-      });
-    });
-
-    describe('get', () => {
-      it('should return the current state', () => {
-        store.update(() => 2);
-
-        expect(store.computedSelector.get({
-          increment: signal(4),
-        })).toBe(6);
-        expect(store.computedSelector.get({
-          increment: signal(2),
-        })).toBe(4);
-        expect(store.computedSelector.inject({
-          increment: signal(2),
-        })()).toBe(4);
-
-        // 3 calls because get does not cache the value
-        expect(store.computedSelectorFn).toHaveBeenCalledTimes(3);
-      });
-
-      it('should return the correct value if somebody is listing', () => {
-        TestBed.runInInjectionContext(() => {
-          const selector = store.computedSelector.inject({
-            increment: signal(2),
-          });
-
-          effect(() => {
-            selector();
-          });
-          TestBed.flushEffects();
-
-          expect(store.computedSelector.get({
-            increment: signal(2),
-          })).toEqual(2);
-
-          // 2 calls as there can be no caching
-          expect(store.computedSelectorFn).toHaveBeenCalledTimes(2);
+          expect(store.selectorWithInputsFn).toHaveBeenCalledTimes(2);
         });
       });
     });
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,40 +1,15 @@
-/**
- * Inspired by https://github.com/tanstack/store but tightly integrated with immerjs and Angular rather than platform independent
- */
-import {CreateSignalOptions, signal, untracked, WritableSignal} from '@angular/core';
+import {Signal, signal, untracked, WritableSignal} from '@angular/core';
 import {Draft, enableMapSet, produce} from 'immer';
-import {Selector} from './selector';
-import {ComputedSelector, SignalInputs} from './computed-selector';
 
 enableMapSet();
 
 export abstract class Store<TState> {
   private readonly _state: WritableSignal<TState>;
+  protected readonly state: Signal<TState>;
 
   protected constructor(initialState: TState) {
     this._state = signal(initialState);
-  }
-
-  protected createSelector<TSelected>(
-    selector: (state: NoInfer<TState>) => TSelected,
-    options: CreateSignalOptions<TSelected> = {},
-  ): Selector<TSelected, TState> {
-    return new Selector<TSelected, TState>(
-      selector,
-      this._state,
-      options,
-    );
-  }
-
-  protected createComputedSelector<TSelected, TInputs extends SignalInputs>(
-    selector: (state: NoInfer<TState>, inputs: TInputs) => TSelected,
-    options: CreateSignalOptions<TSelected> = {},
-  ): ComputedSelector<TSelected, TState, TInputs> {
-    return new ComputedSelector<TSelected, TState, TInputs>(
-      selector,
-      this._state,
-      options,
-    );
+    this.state = this._state.asReadonly();
   }
 
   public update(
